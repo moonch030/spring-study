7fa0e18bb1e9ad3f70a430b12abcc67f6cd35701
moon <cogus2010@naver.com>
2024-03-08 16:07:00 +0900

    2024-03-08 게시글 작성 폼, 디비 insert

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,16 +1,30 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class BoardController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String main(){
+    @Autowired
+    private BoardService boardService;
 
-        return "Hello World!";
+    @GetMapping("/board/write") //lacalhost:8080/board/write
+    public String boardWriteForm(){
+
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
     }
 }
