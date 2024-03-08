8919e793b87b2a0021331d10cf7d9a498ad01a69
moon <cogus2010@naver.com>
2024-03-08 18:55:18 +0900

    2024-03-08 게시글 상세 페이지

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -35,5 +36,12 @@ public class BoardController {
         model.addAttribute("list",boardService.boardList());
 
         return "boardlist";
+    }
+
+    @GetMapping("/board/view") //lacalhost:8080/board/view?id=1
+    public String boardView(Model model,Integer id){
+
+        model.addAttribute("board",boardService.boardView(id));
+        return "boardview";
     }
 }
